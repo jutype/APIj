c2d88e53b43c076b437c8802da323175caa3b1fa
JuanToSeidor <jdtorres@seidoranalytics.com>
2025-07-12 23:19:01 -0300

    initial comit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const PORT = 3000;
+
+// Cargar los códigos postales desde el archivo JSON
+const codigos = JSON.parse(fs.readFileSync('codigos_postales.json', 'utf8'));
+
+app.get('/servicio/:codigo_postal', (req, res) => {
+  const codigo = req.params.codigo_postal;
+  let servicio = null;
+
+  if (codigos.urbano.includes(codigo)) {
+    servicio = 'urbano';
+  } else if (codigos.flash.includes(codigo)) {
+    servicio = 'flash';
+  }
+
+  res.json({ servicio });
+});
+
+app.listen(PORT, () => {
+  console.log(`API escuchando en http://localhost:${PORT}`);
+}); 
